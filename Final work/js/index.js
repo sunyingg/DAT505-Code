4536e24c8635ccec4e09f00d53deb565251a6933
sunyingg <373836546@qq.com>
2019-04-23 14:42:36 +0800

    update

--- Final work/js/index.js
+++ Final work/js/index.js
@@ -9,7 +9,6 @@ var texture;
 var cubesNum = 100;
 var speed = [];
 var cubes = [];
-var cactus =[];
 var raycaster = new THREE.Raycaster();
 var mouseVector = new THREE.Vector3();
 var selectedObject = null;
@@ -34,7 +33,7 @@ function init() {
     renderer.setClearColor(0xffffff, 1);
     document.body.appendChild(renderer.domElement);
 
-//create groups
+//create two groups to better control meshes
     scene = new THREE.Scene();
     group = new THREE.Group();
 
@@ -43,13 +42,29 @@ function init() {
 //set the position of the camera
     camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
     camera.position.set(10, 10, 30);
-    camera.lookAt(0, 0, 0);
+    camera.lookAt(0, 0, 0);//Keep the camera in the middle of the scene
 //set two lights
     var light = new THREE.AmbientLight( '#FFFFE0' ,.8);
   var light1= new THREE.HemisphereLight( '#F0F8FF', 0x080820, 1 );
       light1.position.set(200, 200, 200);
       scene.add(light);
      scene.add( light1);
+
+     // create an AudioListener and add it to the camera
+var listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+var sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+var audioLoader = new THREE.AudioLoader();
+audioLoader.load( 'audio/ghost duet.mp3', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
 
 //create snows
     geometry = new THREE.SphereGeometry(0.09, 1, 1);
@@ -60,8 +75,6 @@ function init() {
         var randomSelection = Math.round(Math.random() * 5);
         // Load a texture
         material = new THREE.PointsMaterial({
-
-            map: texture,
             depthTest: false
         });
 
@@ -112,7 +125,7 @@ function init() {
         mesh2.position.y = -10;
         mesh2.position.z = -10;
         mesh2.rotation.z =10;
-          //mesh2.rotateY(45);
+
         group.add( mesh2 );
 
      var geometry3 = new THREE.BoxGeometry(10, 3, 3);
@@ -121,7 +134,7 @@ function init() {
           mesh3.position.y = -15;
           mesh3.position.z = -10;
           mesh3.rotation.z =-10;
-            //mesh3.rotateY(45);
+
         group.add( mesh3 );
 
     var geometry4 = new THREE.BoxGeometry(3, 20, 3);
@@ -129,7 +142,7 @@ function init() {
           mesh4.position.x = -30;
           mesh4.position.y = -15;
           mesh4.position.z =  -10;
-          //  mesh4.rotateY(45);
+
           group.add(mesh4);
 //create  cactue in the right
           var mesh5 = new THREE.Mesh( geometry2,boxMaterial);
@@ -137,20 +150,20 @@ function init() {
               mesh5.position.y = -15;
               mesh5.position.z = -10;
               mesh5.rotation.z =10;
-                //mesh2.rotateY(45);
+
               group.add( mesh5 );
            var mesh6 = new THREE.Mesh( geometry3,boxMaterial);
                 mesh6.position.x = 30;
                 mesh6.position.y = -15;
                 mesh6.position.z = -10;
                 mesh6.rotation.z =-10;
-                  //mesh3.rotateY(45);
+
               group.add( mesh6);
           var mesh7 = new THREE.Mesh(geometry4, boxMaterial);
                 mesh7.position.x = 35;
                 mesh7.position.y = -20;
                 mesh7.position.z =  -10;
-                //  mesh4.rotateY(45);
+
                 group.add(mesh7);
 //create  cactue in the middle
                 var mesh8 = new THREE.Mesh( geometry2,boxMaterial);
@@ -158,20 +171,20 @@ function init() {
                     mesh8.position.y = -15;
                     mesh8.position.z = -40;
                     mesh8.rotation.z =10;
-                      //mesh2.rotateY(45);
+
                      group.add( mesh8 );
                  var mesh9 = new THREE.Mesh( geometry3,boxMaterial);
                       mesh9.position.x = -5;
                       mesh9.position.y = -13;
                       mesh9.position.z = -40;
                       mesh9.rotation.z =-10;
-                        //mesh3.rotateY(45);
+
                     group.add( mesh9);
                 var mesh10 = new THREE.Mesh(geometry4, boxMaterial);
                       mesh10.position.x = 0;
                       mesh10.position.y = -20;
                       mesh10.position.z =  -40;
-                      //  mesh4.rotateY(45);
+
                       group.add(mesh10);
 
 
@@ -186,7 +199,7 @@ function init() {
 
         action = object.mixer.clipAction(object.animations[0]);
 
-
+        //to change the position and scale
         object.position.x = 10;
         object.position.y = -25;
         object.position.z = -10;
@@ -196,14 +209,16 @@ function init() {
         //add models into groups to control later
           GROUP.add(object);
     });
-
+//set the material of skyBox(Panoramic Image)
     var texturesSky = getTexturesFromAtlasFile("textures/place1.png", 6);
 
     var materialsSky = [];
     for (var i = 0; i < 6; i++) {
         materialsSky.push(new THREE.MeshBasicMaterial({ map: texturesSky[i] }));
     }
-    var skyBox = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), materialsSky);
+    //create the skybox
+    var skygeometry=new THREE.BoxBufferGeometry(1, 1, 1)
+    var skyBox = new THREE.Mesh(skygeometry, materialsSky);
     skyBox.position.x = 0;
      skyBox.position.y = 25;
     skyBox.position.z = -10;
@@ -224,11 +239,13 @@ function getTexturesFromAtlasFile(atlasImgUrl, tilesNum) {
     for (var i = 0; i < tilesNum; i++) {
         textures[i] = new THREE.Texture();
     }
+    //Use the image loader
     var imageObj = new Image();
     imageObj.onload = function () {
         var canvas, context;
         var tileWidth = imageObj.height;
-        for (var i = 0; i < textures.length; i++) {
+        for (var i = 0; i < textures.length; i++) {//to split six pictures.
+          // draw part of it on a canvas
             canvas = document.createElement('canvas');
             context = canvas.getContext('2d');
             canvas.height = tileWidth;
@@ -252,7 +269,7 @@ function onWindowResize() {
 
 function animation() {
     requestAnimationFrame(animation);
-//
+//Set up a loop to play animation
     if (mixers.length > 0) {
         for (var i = 0; i < mixers.length; i++) {
             mixers[i].update(clock.getDelta());
@@ -264,24 +281,14 @@ function animation() {
         cubes[i].rotation.x += 0.02;
         // Rotate the y position of the mesh by 0.02
         cubes[i].rotation.y += 0.02;
-
-
-
         //Move the mesh towards the bottom of the screen
         cubes[i].position.y -= speed[i];
-
-
-
-        //If the mesh passes the bottom of the screen,
         //make it appear on the top.
         if (cubes[i].position.y < 0) {
             cubes[i].position.y = 30;
             cubes[i].position.x = (Math.random() * -40) + 20;
-
-
-
-        }
-
+       }
+//define an angle variable theta
         theta += 0.001;
   camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
   camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
@@ -309,7 +316,7 @@ function onDocumentMouseDown(event) {
         }
     }
 }
-
+//mouse up and the animation wil stop
 function onDocumentMouseUp() {
     action.stop();
 }
